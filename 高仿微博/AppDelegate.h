0ee1419207fdffc440859ccfaeed7bfabd06c2b2
Raychen <751318834@qq.com>
2015-04-07 15:28:30 +0800

    Initial Commit

---
+++ 高仿微博/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  高仿微博
+//
+//  Created by Raychen on 15/4/7.
+//  Copyright (c) 2015年 raychen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 高仿微博/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  高仿微博
+//
+//  Created by Raychen on 15/4/7.
+//  Copyright (c) 2015年 raychen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
